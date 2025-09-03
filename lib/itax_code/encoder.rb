a07dfed2990ba4e7b03b36f23e00cbd317be6870
natydev <natydev@aol.com>
2025-09-03 18:45:39 +0200

    feat: Improve Encoder::Encoder#encode_birthplace
    Use #select instead of #find: this ensure to keep all version of a city 
    and pick the last version at the end.

--- lib/itax_code/encoder.rb
+++ lib/itax_code/encoder.rb
@@ -78,7 +78,8 @@ module ItaxCode
       def encode_birthplace(src = utils.cities, stop: false)
         lookup_key = birthplace.match?(/^\w{1}\d{3}$/) ? "code" : "name"
         place_slug = utils.slugged(birthplace, concise: true)
-        place_item = src.find { |i| place_slug == utils.slugged(i[lookup_key], concise: true) }
+        place_items = src.select { |i| place_slug == utils.slugged(i[lookup_key], concise: true) }
+        place_item = place_items.last if place_items.any?
 
         code = place_item&.[]("code")
         return code if utils.present?(code)
